ea6d5961ec5f2251e8f016d435c1feded7b75ece
Anitha Sathyapriya <ANITHA@users.noreply.github.com>
2017-11-24 00:40:03 +0800

    savewithbackbuttons

--- app/src/main/java/com/example/hp/imageprocessing/RelatedFacesActivity.java
+++ app/src/main/java/com/example/hp/imageprocessing/RelatedFacesActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -16,16 +19,21 @@ import java.util.ArrayList;
 public class RelatedFacesActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
 
     public ArrayList<GridImageClass> bitmapclass;
-    String imgname,userid,folder;
+    public ArrayList<String> bitmapNames;
+    String imgname,userid,folder,name;
+    MenuItem itemCancel,itemSelect;
+    ImageAdapter_Grid adapter;
+    boolean selected = false;
     ProgressBar pb;
+    Toolbar tb,tool;
     TextView tv;
     GridView gv;
     Spinner spinner;
-    String host="http://192.168.48.247/EventTraceWebAppV1/Service1.svc/SearchRelatedImages";
+    String host="http://192.168.0.177/EventTraceWebAppV1/Service1.svc/SearchRelatedImages";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.relatedfaces_layout);
+        setContentView(R.layout.activity_images_in_grid);
         bitmapclass = new ArrayList<>();
 
         Bundle b = getIntent().getExtras();
@@ -33,7 +41,7 @@ public class RelatedFacesActivity extends AppCompatActivity implements AdapterVi
         imgname = b.get("flag").toString();
         userid = b.get("Uid").toString();
         pb = (ProgressBar) findViewById(R.id.progressbar2);
-        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+         tb = (Toolbar) findViewById(R.id.toolbar);
         spinner=(Spinner)findViewById(R.id.categorySpinner);
         spinner.setVisibility(View.INVISIBLE);
 
@@ -62,13 +70,15 @@ public class RelatedFacesActivity extends AppCompatActivity implements AdapterVi
             });
 
 
-        gv = (GridView) findViewById(R.id.gridview2);
+        gv = (GridView) findViewById(R.id.gridview1);
         GridTaskClass gridTask = new GridTaskClass(RelatedFacesActivity.this, gv, pb);
-        gridTask.execute(host+"/"+userid+"/"+imgname);
+        host=host+"/"+userid+"/"+imgname;
+        gridTask.execute(host);
         gv.setOnItemClickListener(RelatedFacesActivity.this);
     }
 
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+    {
 
         Intent intent = new Intent(getApplicationContext(),ImageDetails.class);
         bitmapclass = GridTaskClass.getBitmapclass();
@@ -77,7 +87,77 @@ public class RelatedFacesActivity extends AppCompatActivity implements AdapterVi
         intent.putExtra("Id", imgname);
         intent.putExtra("Uid",userid);
         startActivity(intent);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.grid_menu, menu);
+        itemCancel = menu.findItem(R.id.action_cancel);
+        itemSelect = menu.findItem(R.id.action_select);
+        itemCancel.setVisible(false);
+        return true;
+    }
+    @Override
+
+    public boolean onPrepareOptionsMenu(Menu menu){
+        if(selected){
+            itemSelect.setVisible(false);
+            itemCancel.setVisible(true);
+        }
+        else{
+            itemSelect.setVisible(true);
+            itemCancel.setVisible(false);
+        }
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+        int res_id = item.getItemId();
+        if (res_id == R.id.action_select)
+        {
+            selected  = true;
+            tool = (Toolbar) findViewById(R.id.bottomtoolbar);
+            tool.setVisibility(View.VISIBLE);
+            invalidateOptionsMenu();
+            bitmapNames = new ArrayList<>();
+            bitmapclass = GridTaskClass.getBitmapclass();
+
+            gv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                    if(!bitmapNames.contains(bitmapclass.get(i).name)) {
+                        bitmapNames.add(bitmapclass.get(i).name);
+                        ((CheckableGridView) view).selectImages();
+                        tv.setText(" "+bitmapNames.size()+" selected");
+                    }
+                    else{
+                        bitmapNames.remove(bitmapclass.get(i).name);
+                        ((CheckableGridView) view).unSelectImages(bitmapclass.get(i).relevance);
+                        tv.setText(" "+bitmapNames.size()+" selected");
+                    }
+                }
+            });
+        }
+        else if(res_id == R.id.action_cancel){
+            selected  = false;
+            invalidateOptionsMenu();
+            tool.setVisibility(View.INVISIBLE);
+            bitmapNames.clear();
+            tv.setText("Related Images");
+            setGridViewAdapter();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setGridViewAdapter(){
+        adapter = new ImageAdapter_Grid(RelatedFacesActivity.this, bitmapclass);
+        gv.setAdapter(adapter);
+        gv.setOnItemClickListener(RelatedFacesActivity.this);
     }
 
 
